8d80fa5a00ff0f888a14cd5582a680c57f8f3618
Padmajaa26 <Padmajaa26@example.com>
2024-06-27 11:03:06 +0530

    This is my first Java Project!

---
+++ basic/src/basic/ne.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class ne {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to JFS");
+		System.out.println("MVIT");
+		System.out.println("TNSIF");
+
+	}
+
+}
