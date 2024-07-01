5c490f4ca50b8e6f614656451602d5a1359065bd
Padmajaa26 <Padmajaa26@example.com>
2024-07-01 16:38:24 +0530

    Progress

---
+++ basic/src/Day3/class_object.java
@@ -0,0 +1,13 @@
+package Day3;
+
+public class class_object {
+	
+	public static void main(String[] args) { 
+	//object creation  
+	OopsConceptDemo obj = new OopsConceptDemo(); 
+	obj.setSerialNum(101); 
+	obj.setName("Pooja");  
+	obj.setAge(20); 
+	System.out.println(obj); 
+	} 
+	}
